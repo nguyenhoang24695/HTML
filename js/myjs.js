04d32ca806501c4f430e854dd6e220e902189984
Nguyen Hoang <hoangnvd00536@fpt.edu.vn>
2017-10-24 02:04:19 +0700

    Register with reponsive

--- js/myjs.js
+++ js/myjs.js
@@ -1,4 +1,3 @@
-
 var userName = document.forms["my-form"].elements["userName"];
 var passWord = document.forms["my-form"].elements["passWord"];
 var repassWord = document.forms["my-form"].elements["repassWord"];
@@ -44,28 +43,62 @@ repassWord.onkeyup = function (){
 	}
 }
 
-
-//Lấy giá trị của gender
-var genderSelect;
-var genderValue;
-function genderChange() {
-	genderSelect=gender.selectedIndex;
-	genderValue = gender.option[genderSelect].value;
+var AUTHENTICATION_URL = "https://youtube-api-challenger2.appspot.com/members";
+var btnregister = document.getElementById("register");
+btnregister.onclick = function(){
+	alert(1);
+	var user = document.forms["my-form"].elements["userName"].value;
+	var pass = document.forms["my-form"].elements["passWord"].value;
+	var name = document.forms["my-form"].elements["fullName"].value;
+	var mail = document.forms["my-form"].elements["email"].value;
+	var birth = document.forms["my-form"].elements["birthDay"].value;
+	var gen = document.getElementById("gender");
+	var data = {
+		"data":{ 
+		    "type":"Member",
+		    "attributes":{ 
+		        "username": user,
+		        "password": pass,
+		        "fullName":name,
+		        "email":mail,
+		        "birthDay":birth,
+		        "gender":gen.selectedIndex
+		      }
+		    }
+	};
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+	if (this.readyState == 4 && this.status == 200) {
+		document.body.innerHTML = this.responseText;
+	}
+};
+	xhttp.open("POST",AUTHENTICATION_URL,true);
+	xhttp.send(JSON.stringify(data));
 }
 
 
-agree.addEventListener("click", function(){alert(genderValue)
-	// if (genderSelect = "2") {
-	// 	genderMsg.innerHTML = "Bạn chưa chọn giới tính"
-	// 	genderMsg.style = "color : red"
-	// }else{
-	// 	genderMsg.innerHTML = "OK"
-	// 	genderMsg.style = "color : green"
-	// }
-}) 
-agree.addEventListener("click", function(){
-	if (email.value.length == 0) {
-		emailMsg.innerHTML = "Bạn chưa nhập Email"
-		emailMsg.style = "color: red"
-	}
-})
+
+// //Lấy giá trị của gender
+// var genderSelect;
+// var genderValue;
+// function genderChange() {
+// 	genderSelect=gender.selectedIndex;
+// 	genderValue = gender.option[genderSelect].value;
+// }
+
+
+// agree.addEventListener("click", function(){alert(genderValue)
+// 	// if (genderSelect = "2") {
+// 	// 	genderMsg.innerHTML = "Bạn chưa chọn giới tính"
+// 	// 	genderMsg.style = "color : red"
+// 	// }else{
+// 	// 	genderMsg.innerHTML = "OK"
+// 	// 	genderMsg.style = "color : green"
+// 	// }
+// }) 
+// agree.addEventListener("click", function(){
+// 	if (email.value.length == 0) {
+// 		emailMsg.innerHTML = "Bạn chưa nhập Email"
+// 		emailMsg.style = "color: red"
+// 	}
+// })
